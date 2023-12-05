9f9e714719800dbade268a88e8d25ed30947636d
Gustebobben <gustav.alv07@gmail.com>
2023-12-05 15:12:42 +0100

    .

---
+++ Gustebobben/alder.js
@@ -0,0 +1,10 @@
+const birthDate = new Date('2007-05-16');
+        
+function updateAge() {
+    const currentDate = new Date();
+    const ageInMilliseconds = currentDate - birthDate;
+    const ageInYears = ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
+    document.getElementById('ageDisplay').textContent = `${ageInYears.toFixed(10)}`;
+}
+updateAge();
+setInterval(updateAge, 1);
